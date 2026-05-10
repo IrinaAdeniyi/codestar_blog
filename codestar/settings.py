b8924494f5bebbc27e12df88ab1ec7a35daa4815
IrinaAdeniyi <irina.adeniyi@yahoo.com>
2026-05-10 14:08:14 +0100

    Fix database configuration

--- codestar/settings.py
+++ codestar/settings.py
@@ -87,7 +87,9 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3'
+    )
 }
 
 
